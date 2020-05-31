d96c8336b03eef835f0d36cada6582f276efee44
Manikanta <manikanta.m@orotron.com>
2020-05-31 23:44:05 +0530

    Updates

--- js/tools.js
+++ js/tools.js
@@ -1,13 +1,25 @@
 new Vue({
   el: '#app',
   data: {
+    searchString: '',
     tools: [],
+    allTools: [],
+    sort: '',
   },
   created() {
     axios
       .get("../data/tools.json")
       .then(response => {
         this.tools = response.data;
+        var getAllTools = response.data.map(o => o.list).flat();
+        this.allTools = getAllTools;
+
       });
+  },
+  computed: {
+    filteredList: function () {
+      const searchString = this.searchString.toLowerCase();
+      return this.tools.filter(item => item.name.toLowerCase().includes(searchString));
+    }
   }
 });
