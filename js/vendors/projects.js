05a6f5b50420ae54effd7dd766a2a4e394ada45d
Mani <manikantanani14@gmail.com>
2021-07-30 15:27:12 +0530

    UI updates

--- js/vendors/projects.js
+++ js/vendors/projects.js
@@ -1,39 +1,11 @@
-(function ($) {
-  'use strict';
-
-  $(window).on('load', function () {
-    $('.portfolio-filter ul li').on('click', function () {
-      $('.portfolio-filter ul li').removeClass('active');
-      $(this).addClass('active');
-
-      var data = $(this).attr('data-filter');
-      $workGrid.isotope({
-        filter: data
-      });
-    });
-
-    if (document.getElementById('portfolio')) {
-      var $workGrid = $('.portfolio-grid').isotope({
-        itemSelector: '.all',
-        percentPosition: true,
-        masonry: {
-          columnWidth: '.all'
-        }
-      });
-    }
-  });
-})(jQuery);
-
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     projects: [],
   },
   created() {
-    axios
-      .get("./data/projects.json")
-      .then(response => {
-        this.projects = response.data;
-      });
-  }
+    axios.get("./data/projects.json").then((response) => {
+      this.projects = response.data;
+    });
+  },
 });
