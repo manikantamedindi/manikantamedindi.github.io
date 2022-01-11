1a658bd52d7651b97ee8224d2bd940b7953f6220
Mani <manikanta.m@labsquire.com>
2022-01-11 22:54:58 +0530

    Resume updated

--- js/app.js
+++ js/app.js
@@ -7,50 +7,57 @@ $(window).scroll(function () {
     $("#main-navbar").removeClass("affix");
   }
 });
-// Copyright year dynamic 
-$('.c-year').text(new Date().getFullYear());
-// AOS Initialization 
+// Copyright year dynamic
+$(".c-year").text(new Date().getFullYear());
+// AOS Initialization
 AOS.init({
-  disable: 'mobile',
-  duration: 1000
-})
+  disable: "mobile",
+  duration: 1000,
+});
 
 // AOS Refresh
-$(window).on('load', function () {
+$(window).on("load", function () {
   AOS.refresh();
 });
 
-// Changes Img to SVG 
-$('img.svg').each(function () {
+// Changes Img to SVG
+$("img.svg").each(function () {
   var $img = $(this);
-  var imgID = $img.attr('id');
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-  $.get(imgURL, function (data) {
-    var $svg = $(data).find('svg');
-    if (typeof imgID !== 'undefined') {
-      $svg = $svg.attr('id', imgID);
-    }
-    if (typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass + ' replaced-svg');
-    }
-    $svg = $svg.removeAttr('xmlns:a');
-    $img.replaceWith($svg);
-  }, 'xml');
+  var imgID = $img.attr("id");
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(
+    imgURL,
+    function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgID !== "undefined") {
+        $svg = $svg.attr("id", imgID);
+      }
+      if (typeof imgClass !== "undefined") {
+        $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      $img.replaceWith($svg);
+    },
+    "xml"
+  );
 });
 
-
 // Add smooth scrolling to all links
-$(".scroll").on('click', function (event) {
+$(".scroll").on("click", function (event) {
   if (this.hash !== "") {
     event.preventDefault();
     var hash = this.hash;
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function () {
-      window.location.hash = hash;
-    });
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
   }
 });
 
@@ -91,3 +98,23 @@ $(".scroll").on('click', function (event) {
 //   }
 //   document.addEventListener('DOMContentLoaded', loadbar, false);
 // }());
+
+var lang = {
+  html: "100%",
+  css: "90%",
+  javascript: "70%",
+  php: "55%",
+  angular: "65%",
+};
+
+var multiply = 4;
+
+$.each(lang, function (language, pourcent) {
+  var delay = 700;
+
+  setTimeout(function () {
+    $("#" + language + "-pourcent").html(pourcent);
+  }, delay * multiply);
+
+  multiply++;
+});
